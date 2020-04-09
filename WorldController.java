0b4592060798c18d68720f3756f1ec482b1c08e8
Ben Chen <49625823+bchen21@users.noreply.github.com>
2020-04-08 21:22:27 -0400

    imports and extend GraphicsProgram

--- WorldController.java
+++ WorldController.java
@@ -1,5 +1,9 @@
+import java.awt.Color;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
 
-public class WorldController {
+public class WorldController extends GraphicsProgram {
 
   public void run() {
     /* more code here */
