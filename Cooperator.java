341c05610934abf944edfd1fc03062da42c2b5e7
Ben Chen <bchen21@deerfield.edu>
2020-04-14 15:45:54 -0400

    updated cooperator and defector

--- Cooperator.java
+++ Cooperator.java
@@ -1,4 +1,10 @@
+import javafx.scene.paint.Color;
 
-public class Cooperator {
-
+public class Cooperator extends Creature{
+	
+	public Cooperator(Location l, World w) {
+		super(l,w);
+		myColor = Color.GREEN;
+	}
+	
 }
--- Defector.java
+++ Defector.java
@@ -1,4 +1,10 @@
+import javafx.scene.paint.Color;
 
-public class Defector {
-
+public class Defector extends Creature{
+	
+	public Defector(Location l, World w) {
+		super(l,w);
+		myColor = Color.PURPLE;
+	}
+	
 }
