3bebdeaa2fe9fc4d33fdb64fc211837925aacad7
wsavage20 <63025100+wsavage20@users.noreply.github.com>
2020-04-21 15:06:46 -0400

    Update Defector.java

--- Defector.java
+++ Defector.java
@@ -1,10 +1,24 @@
-import javafx.scene.paint.Color;
+import java.awt.Color;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.util.ArrayList;
 
 public class Defector extends Creature{
 	
 	public Defector(Location l, World w) {
 		super(l,w);
-		myColor = Color.PURPLE;
+		myColor = Color.red;
+	}
+	public void interact() {
+		for(Creature x : myNeighbors) {
+			if(x.getMyColor() == Color.GREEN)
+				myScore+=5;
+			else if(x.getMyColor() == Color.RED) {
+				myScore+=1;
+			}
+		}
 	}
 	
+		
 }
