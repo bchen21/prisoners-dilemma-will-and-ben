e8c56973eb97206c2c7a911a36869452b281c15a
wsavage20 <63025100+wsavage20@users.noreply.github.com>
2020-04-21 15:06:04 -0400

    Update Cooperator.java

--- Cooperator.java
+++ Cooperator.java
@@ -1,4 +1,8 @@
-import javafx.scene.paint.Color;
+import java.awt.Color;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.util.ArrayList;
 
 public class Cooperator extends Creature{
 	
@@ -7,4 +11,20 @@ public class Cooperator extends Creature{
 		myColor = Color.GREEN;
 	}
 	
+	public void interact() {
+		for(Creature x : myNeighbors) {
+			if(x.getMyColor() == Color.GREEN)
+				myScore+=3;
+		}
+	}
+	
+	public void seeWhoIsBest() {
+		int highScore = 0;
+		int bestNeighborIndex = -1;
+		for(int i = 0; i < 4; i++) {
+			if(myNeighbors.get(i).getMyScore() > myScore) {
+				
+			}
+		}
+	}
 }
