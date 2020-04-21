e00d3fd686cbbc0c18302e36dd947bde49ab6c46
wsavage20 <63025100+wsavage20@users.noreply.github.com>
2020-04-21 15:56:42 -0400

    Update World.java

--- World.java
+++ World.java
@@ -25,7 +25,17 @@ public class World {
 			x.findMyNeighbors(x.getMyLocation());
 			x.interact();
 		}
+		for(Creature x : creatureList) {
+			x.seeWhoIsBest();
+		}
+		for(int i = 0; i < creatureList.size(); i++) {
+			Creature x = creatureList.get(i);
+			if(x.willIChange())
+				swapPlayer(i, x.getMyColor(), x.getMyLocation(), x.getMyWorld());
+				
+		}
 	}
+	
 	public int getWidth() {
 		return width;
 	}
@@ -43,6 +53,15 @@ public class World {
 	}
 	public void setCreatureList(ArrayList<Creature> creatureList) {
 		this.creatureList = creatureList;
+	}
+	
+	public void swapPlayer(int index, Color color, Location xy, World world) {
+		creatureList.remove(index);
+		if(color == Color.GREEN) {
+			creatureList.add(index, new Defector(xy, world));
+		} else {
+			creatureList.add(index, new Cooperator(xy, world));
+		}
 	}
 
 	@Override
