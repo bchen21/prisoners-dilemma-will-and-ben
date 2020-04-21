5772068c46a153ba8c5f3c1bb4317576b9a18245
wsavage20 <63025100+wsavage20@users.noreply.github.com>
2020-04-21 15:56:08 -0400

    Update Defector.java

--- Defector.java
+++ Defector.java
@@ -20,5 +20,20 @@ public class Defector extends Creature{
 		}
 	}
 	
+	public void seeWhoIsBest() {
+		int highScore = myScore;
+		int bestNeighborIndex = -1;
+		for(int i = 0; i < myNeighbors.size(); i++) {
+			if(myNeighbors.get(i).getMyScore() > highScore) {
+				bestNeighborIndex = i;
+				highScore = myNeighbors.get(i).getMyScore();
+			}
+		}
+		if(bestNeighborIndex != -1) {
+			if(myNeighbors.get(bestNeighborIndex).getMyColor() == Color.GREEN) {
+				willChange = true;
+			}
+		}
+	}
 		
 }
